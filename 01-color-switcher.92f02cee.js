1e71e4ca1872ba2e3e7b4df0261a3664c07c4a63
Valerii-Postrybailo <99152023+Valerii-Postrybailo@users.noreply.github.com>
2022-08-25 21:34:45 +0000

    Deploying to gh-pages from @ Valerii-Postrybailo/goit-js-hw-09@45501d458b148a54e9cfcd05b5bbc84d3587fcc8 🚀

---
+++ 01-color-switcher.92f02cee.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");function l(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}let d=null;t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,l(),console.log("start"),d=setInterval((()=>{l(),console.log("Go!")}),1e3)})),e.addEventListener("click",(()=>{e.disabled=!0,clearInterval(d),t.disabled=!1,console.log(`Interval with id ${d} has stopped!`)}));
+//# sourceMappingURL=01-color-switcher.92f02cee.js.map
