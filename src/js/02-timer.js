53594af3e2176fd3675c7fceedde3b3f50edd1d9
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-08-28 22:48:42 +0300

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,6 +6,19 @@ import "flatpickr/dist/flatpickr.min.css";
 const startTimerBtn = document.querySelector("button[data-start]")
 startTimerBtn.disabled = true
 
+const timePicker = document.querySelector("#datetime-picker")
+
+timerValue = {
+  days: document.querySelector("[data-days]"),
+  hours: document.querySelector("[data-hours]"),
+  minutes: document.querySelector("[data-minutes]"),
+  seconds: document.querySelector("[data-seconds]")
+}
+
+let timerId = null;
+
+/////////////////////////////////////////////////////////
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -14,47 +27,62 @@ const options = {
   onClose(selectedDates) {
     console.log(selectedDates[0]);
     console.log(new Date())
-  
 
-    startTimerBtn.addEventListener
-startTimerBtn.addEventListener('click', () => {
-    const timer = { 
-  start() {
-    const startTime = selectedDates[0];
-
-    setInterval(() => { 
-      console.log(startTime - convertMs(1000))
-    }, 1000)
-  }
-}
-  }
-)
     if (selectedDates[0] < new Date()) {
       startTimerBtn.disabled = true
       window.alert("Please choose a date in the future")
     } else {
       startTimerBtn.disabled = false
+      startTimerBtn.addEventListener("click", () => { changeTimerValue(selectedDates[0]) })
     }
   },
 };
 
-const timePicker = document.querySelector("#datetime-picker")
-console.log(timePicker)
-
 flatpickr(timePicker, options)
 
-startTimerBtn.addEventListener('click', () => {
-    const timer = { 
-  start() {
-    const startTime = selectedDates[0];
+function changeTimerValue(selectedTime) {
+  const timer = {
+    start() {
+      const startTime = selectedTime
+      timerId = setInterval(() => { 
+        const currentTime = Date.now()
+      
+        const deltaTime = currentTime - startTime
+        const { days, hours, minutes, seconds } = convertMs(deltaTime)
 
-    setInterval(() => { 
-      console.log(startTime - convertMs(1000))
-    }, 1000)
+        timerValue.days.textContent = days;
+        timerValue.hours.textContent = hours;
+        timerValue.minutes.textContent = minutes;
+        timerValue.seconds.textContent = seconds;
+        // console.log(days)
+        // console.log(typeof (days))
+        
+        if (days === "01") {
+          timerValue.days.textContent = "00"
+        }
+
+        if (hours === "01") {
+          timerValue.hours.textContent = "00"
+        }
+
+        if (minutes === "01") {
+          timerValue.minutes.textContent = "00"
+        }
+
+        if (seconds <= 0) {
+          console.log("I'm worked")
+          clearInterval(timerId)
+        }
+      }, 1000)
+    }
   }
+
+  timer.start()
 }
-  }
-)
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -64,17 +92,13 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day)*- 1);
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour) * -1);
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute)* -1);
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second)* -1);
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,20 @@
+const form = document.querySelector(".form")
+
+const firstDelay = document.querySelector('input[name="delay"]')
+console.log(firstDelay)
+
+const delayStep = document.querySelector('input[name="step"]')
+console.log(delayStep)
+
+const amount = document.querySelector('input[name="amount"]')
+console.log(delayStep)
+
+// form.addEventListener("submit", createPromise(position, delay));
+
+// function submitForm(event) {
+//   event.preventDefault();
+// }
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
