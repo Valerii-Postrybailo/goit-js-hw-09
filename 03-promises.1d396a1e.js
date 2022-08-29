b747eea380ceb69b1cca9266a7152d02c14ff913
Valerii-Postrybailo <99152023+Valerii-Postrybailo@users.noreply.github.com>
2022-08-29 23:41:55 +0000

    Deploying to gh-pages from @ Valerii-Postrybailo/goit-js-hw-09@4581588c5c181de155873f7e21def76e47d50edf 🚀

--- 03-promises.1d396a1e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form");const e=document.querySelector('input[name="delay"]');console.log(e);const o=document.querySelector('input[name="step"]');console.log(o);document.querySelector('input[name="amount"]');console.log(o);
-//# sourceMappingURL=03-promises.1d396a1e.js.map
--- 03-promises.922740e2.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector(".form");var e=document.querySelector('input[name="delay"]');console.log(e);var o=document.querySelector('input[name="step"]');console.log(o);document.querySelector('input[name="amount"]');console.log(o)}();
-//# sourceMappingURL=03-promises.922740e2.js.map
